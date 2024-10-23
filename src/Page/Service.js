ecd1879601e1b221eee9cd9333bffb1b1d5eade2
abdul061 <godofcoc346346@gmail.com~>
2024-10-23 19:30:41 +0530

    abdul

--- src/Page/Service.js
+++ src/Page/Service.js
@@ -258,7 +258,7 @@ const Service = () => {
 
       <Banner />
 
-
+      <div>
         <div className="w-layout-grid udesly-cards-grid udesly-3-columns">
           {/* Card 1 */}
           <ServiceCard
@@ -286,7 +286,7 @@ const Service = () => {
               description="We offer internships that bridge the gap between education and real-world experience."
             />
         </div>
-
+      </div>
 
     </>
   );
